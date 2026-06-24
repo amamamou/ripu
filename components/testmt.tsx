a1d6647e10f34e744f5b9d5661604bf24908a605
amamamou <amamoumaram88@gmail.com>
2026-06-24 01:35:52 +0100

    file r

--- components/testmt.tsx
+++
@@ -1,2 +0,0 @@
-export { TestimonialsSection as TestimonialsSectione } from "@/components/ripu25/testimonials-section"
-export { RIPU25SyncSections, Ripu25Section as Ripu25GallerySection } from "@/components/ripu25-section"
